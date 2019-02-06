adb18ce059d20c60c48cfcec8ae53a818ce169f2
Tudor Hutu <hutu.tudor@yahoo.com>
2019-02-06 10:39:15 +0200

    Changed the toast position

--- frontend/src/services/notificationService.js
+++ frontend/src/services/notificationService.js
@@ -1,14 +1,18 @@
 import { toast } from 'react-toastify';
 import * as lang from './langService';
 
+const options = {
+	position: toast.POSITION.BOTTOM_RIGHT
+};
+
 export const error = () => {
-	toast.error(lang.get('messages.error'));
+	toast.error(lang.get('messages.error'), options);
 };
 
 export const errorN = name => {
-	toast.error(lang.get(name));
+	toast.error(lang.get(name), options);
 };
 
 export const success = name => {
-	toast.success(lang.get(name));
+	toast.success(lang.get(name), options);
 };

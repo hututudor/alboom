39833d0137775b436bb00a8d1884d549786d15cb
Tudor Hutu <hutu.tudor@yahoo.com>
2019-02-10 09:57:55 +0200

    Updated transitions to support appear

--- frontend/src/services/transitionService.js
+++ frontend/src/services/transitionService.js
@@ -1,14 +1,14 @@
 export const transitions = [
-  'none',
-  'zoomIn',
-  'zoomOut',
-  'slideUp',
-  'slideDown',
-  'slideLeft',
-  'slideRight',
-  'bounceUp',
-  'bounceDown',
-  'bounceLeft',
-  'bounceRight',
-  'fadeIn'
+	'none',
+	'zoomIn',
+	'zoomOut',
+	'slideUp',
+	'slideDown',
+	'slideLeft',
+	'slideRight',
+	'bounceUp',
+	'bounceDown',
+	'bounceLeft',
+	'bounceRight',
+	'fadeIn'
 ];

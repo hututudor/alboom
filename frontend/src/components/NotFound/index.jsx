56e5025db5d25dbdf29b0e5794c543cbef7524ba
Tudor Hutu <hutu.tudor@yahoo.com>
2019-02-10 19:25:30 +0200

    Added a nice 404 page

--- frontend/src/components/NotFound/index.jsx
+++ frontend/src/components/NotFound/index.jsx
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
 import Lang from '../hoc/Lang';
+import './style.scss';
 
 class NotFound extends Component {
 	render() {
 		return (
-			<div>
-				<p>404</p>
-				<p>
-					<Lang>404.message</Lang>
-				</p>
+			<div className="page_404">
+				<div>
+					<p>404</p>
+					<p>
+						<Lang>404.message</Lang>
+					</p>
+				</div>
 			</div>
 		);
 	}

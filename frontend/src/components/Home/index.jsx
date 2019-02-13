ef9a5291d8fa21c2fc2d7adfdccd12e3d80f43b4
hututudor <hutu.tudor@yahoo.com>
2019-02-13 19:18:35 +0200

    Fixes for home

--- frontend/src/components/Home/index.jsx
+++ frontend/src/components/Home/index.jsx
@@ -3,26 +3,28 @@ import Navbar from '../Navbar';
 import Logo from '../hoc/Logo';
 import './style.scss';
 import Lang from '../hoc/Lang';
+import { Segment } from 'semantic-ui-react';
 
 class Home extends Component {
-	render() {
-		return (
-			<React.Fragment>
-				<Navbar />
-				<div className="imgheader">
-					<div>
-						<Logo size={50} />
-						<span>
-							<Lang>home.title</Lang>
-						</span>
-						<span>
-							<Lang>home.subtitle</Lang>
-						</span>
-					</div>
-				</div>
-			</React.Fragment>
-		);
-	}
+  render() {
+    return (
+      <React.Fragment>
+        <Navbar />
+        <div className="img__header">
+          <div>
+            <Logo size={50} />
+            <span>
+              <Lang>home.title</Lang>
+            </span>
+            <span>
+              <Lang>home.subtitle</Lang>
+            </span>
+          </div>
+        </div>
+        <Segment vertical>sdd</Segment>
+      </React.Fragment>
+    );
+  }
 }
 
 export default Home;

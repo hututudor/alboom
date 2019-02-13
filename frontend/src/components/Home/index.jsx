7aa7330629d3e36f510e455351808165bd3d3841
Tudor Hutu <hutu.tudor@yahoo.com>
2019-02-13 20:28:51 +0200

    Added a new section for home page

--- frontend/src/components/Home/index.jsx
+++ frontend/src/components/Home/index.jsx
@@ -3,28 +3,117 @@ import Navbar from '../Navbar';
 import Logo from '../hoc/Logo';
 import './style.scss';
 import Lang from '../hoc/Lang';
-import { Segment } from 'semantic-ui-react';
+import {
+	Segment,
+	Divider,
+	Button,
+	Icon,
+	Card,
+	Container
+} from 'semantic-ui-react';
 
 class Home extends Component {
-  render() {
-    return (
-      <React.Fragment>
-        <Navbar />
-        <div className="img__header">
-          <div>
-            <Logo size={50} />
-            <span>
-              <Lang>home.title</Lang>
-            </span>
-            <span>
-              <Lang>home.subtitle</Lang>
-            </span>
-          </div>
-        </div>
-        <Segment vertical>sdd</Segment>
-      </React.Fragment>
-    );
-  }
+	render() {
+		return (
+			<React.Fragment>
+				<Navbar />
+				<div className="img__header">
+					<div>
+						<Logo size={50} />
+						<span>
+							<Lang>home.title</Lang>
+						</span>
+						<span>
+							<Lang>home.subtitle</Lang>
+						</span>
+					</div>
+				</div>
+				<Segment vertical style={{ textAlign: 'center', padding: '7em 2em' }}>
+					<h1>
+						<Lang>home.cli</Lang>
+					</h1>
+					<h4>
+						<b>
+							<Lang>home.clidesc</Lang>
+						</b>
+					</h4>
+					<Button color="blue" icon labelPosition="left">
+						<Icon name="download" />
+						<Lang>home.clidown</Lang>
+					</Button>
+				</Segment>
+				<div className="pros">
+					<h1 style={{ marginBottom: '2em' }}>
+						<Lang>home.pros.title</Lang>
+					</h1>
+					<Container>
+						<Card.Group stackable centered itemsPerRow={3}>
+							<Card color="green">
+								<Card.Content style={{ textAlign: 'center' }}>
+									<h1>
+										<Icon name="users" />
+									</h1>
+									<h4>
+										<Lang>home.pros.1</Lang>
+									</h4>
+								</Card.Content>
+							</Card>
+							<Card color="green">
+								<Card.Content style={{ textAlign: 'center' }}>
+									<h1>
+										<Icon name="lightning" />
+									</h1>
+									<h4>
+										<Lang>home.pros.2</Lang>
+									</h4>
+								</Card.Content>
+							</Card>
+							<Card color="green">
+								<Card.Content style={{ textAlign: 'center' }}>
+									<h1>
+										<Icon name="cogs" />
+									</h1>
+									<h4>
+										<Lang>home.pros.3</Lang>
+									</h4>
+								</Card.Content>
+							</Card>
+							<Card color="green">
+								<Card.Content style={{ textAlign: 'center' }}>
+									<h1>
+										<Icon name="flag" />
+									</h1>
+									<h4>
+										<Lang>home.pros.4</Lang>
+									</h4>
+								</Card.Content>
+							</Card>
+							<Card color="green">
+								<Card.Content style={{ textAlign: 'center' }}>
+									<h1>
+										<Icon name="phone" />
+									</h1>
+									<h4>
+										<Lang>home.pros.5</Lang>
+									</h4>
+								</Card.Content>
+							</Card>
+							<Card color="green">
+								<Card.Content style={{ textAlign: 'center' }}>
+									<h1>
+										<Icon name="folder" />
+									</h1>
+									<h4>
+										<Lang>home.pros.6</Lang>
+									</h4>
+								</Card.Content>
+							</Card>
+						</Card.Group>
+					</Container>
+				</div>
+			</React.Fragment>
+		);
+	}
 }
 
 export default Home;
